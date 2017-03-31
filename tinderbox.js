7ea62b60f0c1b887ff2a40d1142116349fb8bfc6
JuliaYeomans <julia.yeomans@icloud.com>
2017-03-31 11:32:33 +0200

    last version

--- tinderbox.js
+++ tinderbox.js
@@ -1,17 +1,16 @@
-let witch = document.querySelector("#witch");
+///*java*//let witch = document.querySelector("#witch");
 let soldier = document.querySelector("#soldier");
 let tree = document.querySelector("#tree");
 let dog = document.querySelector("#dog");
 let chest = document.querySelector("#chest");
+let ochest = document.querySelector("#ochest");
 let buttonOne = document.querySelector("#buttonOne");
 let music = document.querySelector("#music");
 let buttonTwo = document.querySelector("#buttonTwo");
 
-let coins = document.querySelectorAll("#coins")
-
 
 /*text*/
-var myString = "Once upon a time there was a poor foot soldier walking along a path. He came across a witch standing by a tree, who told him that if he entered the tree he would face three dogs guarding chests filled with gold. The poor soldier was feeling brave, so he decided to climb down into the tree.";
+var myString = "Once upon a time there was a poor foot soldier walking along a path. He came across a witch standing by a tree, who told him that if he entered the tree he would face a dog guarding a chest filled with gold. The poor soldier was feeling brave, so he decided to climb down into the tree. !!CLICK TO COLLECT THE COINS!!";
 var myArray = myString.split("");
 var loopTimer;
 
@@ -44,38 +43,44 @@ function removeClassEnterTree(){
     soldier.classList.remove("enterTree");
 }
 /*chest*/
-console.log("Open the chest!");
-buttonTwo.addEventListener("click", openChest);
+console.log("shake the chest!");
+buttonTwo.addEventListener("click", shakeChest);
+
+function shakeChest(){
+    console.log("Chest Shakes");
+    chest.classList.add("shakeChest");
+	chest.addEventListener("animationend",removeShakeChest);
+}
+
+function removeShakeChest(){
+	console.log("i am being called");
+	chest.style.display="none";
+	ochest.style.display="block";
+}
+chest.addEventListener("openchest", openChest);
 
 function openChest(){
-    console.log("Chest Opens");
+    console.log("I have been called");
     chest.classList.add("openChest");
 }
 
-chest.addEventListener("animationend", removeClassOpenChest);
-
-function removeClassOpenChest(){
-    console.log("I have been called");
-    chest.classList.remove("openChest");
-}
-
-/*openchest*/
-
 /*coins*/
 
-let coinsClicked = 0;
 
-coins.forEach(addEventListener);
+        let coins = document.querySelectorAll(".coins img");
+        let coinsClicked = 0;
 
-        function addEventListener(coins){
-            coins.addEventListener("click", acoinsClicked);
+        coins.forEach(makeMagic);
 
+        function makeMagic(one){
+            one.addEventListener("click", acoinsClicked);
         }
         function acoinsClicked(){
-            this.classList.add("coins");
-            let picked=document.querySelectorAll(".coins");
+            this.classList.add("fade");
+            console.log("i have been clicked", this);
+
+            let picked=document.querySelectorAll(".fade");
             if(picked.length==3){
                 console.log("3 flowers clicked");
             }
         }
-
